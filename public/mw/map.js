7d901b786ead35182b00f801e3ad2b40139dfbe3
muskaannarula22 <muskaannarula22@vt.edu>
2022-04-12 21:48:49 -0400

    map dialog box in index.html

---
+++ public/mw/map.js
@@ -0,0 +1,35 @@
+
+// // map modal for action
+// var modal = document.getElementById("modal-map");
+
+// // opens map 
+// var mapOpen = document.getElementById("modal-map-open");
+
+// // close map
+// var closeMap = document.getElementsById("map-back");
+
+
+// open modal
+function openMap() {
+    var modal = document.getElementById("modal-map");
+    modal.style.display = "block";
+}
+
+// open modal
+function closeMap() {
+    var modal = document.getElementById("modal-map");
+    modal.style.display = "none";
+}
+
+
+// // open modal
+// mapOpen.onclick = function() {
+//     console.log("Open");
+//   modal.style.display = "block";
+// }
+
+// // close modal
+// closeMap.onclick = function() {
+//     console.log("Close");
+//   modal.style.display = "none";
+// }
