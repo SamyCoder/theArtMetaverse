dab1d85bca86e77ef67dc9265d239cb7aa3e9f51
muskaannarula22 <muskaannarula22@vt.edu>
2022-04-29 15:54:03 -0400

    comments on navigation

--- public/mw/map.js
+++ public/mw/map.js
@@ -6,18 +6,23 @@ function openMap() {
     modal.style.display = "block";
 }
 
-// open modal
+// close modal
 function closeMap() {
     var modal = document.getElementById("modal-map");
     modal.style.display = "none";
 }
 
+// teleport to first checkpoint on checkoint 
+// on map being clicked
 function viewCheckpointEnd() {
     var modal = document.getElementById("modal-map");
     modal.style.display = "none";
     document.getElementById('end-checkpoint').setAttribute('set_bind','true');
     
 }
+
+// teleport to second checkpoint on checkoint 
+// on map being clicked
 function viewCheckpointTop() {
     var modal = document.getElementById("modal-map");
     modal.style.display = "none";
